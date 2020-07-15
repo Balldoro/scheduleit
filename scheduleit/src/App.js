abe8c954a356b0622c3d80a71fa8dbf612db9a99
Marek Miklaszewski <mar.mik1@wp.pl>
2020-07-15 12:08:04 +0200

    Set up basic store structure for auth

--- scheduleit/src/App.js
+++ scheduleit/src/App.js
@@ -1,14 +1,17 @@
-import React from "react"
-import Auth from "./components/Auth/Auth"
-import { GlobalStyles } from "./GlobalStyles"
+import React from "react";
+import Auth from "./components/Auth/Auth";
+import { GlobalStyles } from "./GlobalStyles";
+import { Provider } from "react-redux";
+import store from "./store/store";
 
 function App() {
-    return(
-    <>
-        <GlobalStyles />
-        <Auth />
-        <h1>Hello from App!</h1>
-    </>)
+  return (
+    <Provider store={store}>
+      <GlobalStyles />
+      <Auth />
+      <h1>Hello from App!</h1>
+    </Provider>
+  );
 }
 
-export default App
+export default App;
---
+++ scheduleit/src/store/reducers/authReducer.js
@@ -0,0 +1,8 @@
+function authReducer(state = { user: null }, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
+
+export default authReducer;
