ebe8050892ef4a4e5e278f136435c508112628ac
Marek Miklaszewski <mar.mik1@wp.pl>
2020-07-16 23:53:29 +0200

    Add actions, reducer and constants for SignUp

--- scheduleit/src/store/actions/authActions.js
+++ scheduleit/src/store/actions/authActions.js
@@ -1,3 +1,28 @@
-export function createUser() {
-  return (dispatch) => {};
+import { auth, db } from "../../firebase";
+import {
+  REQUEST_CREATE_USER,
+  SUCCESS_CREATE_USER,
+} from "../constants/authConstants";
+
+export function requestCreateUser() {
+  return { type: REQUEST_CREATE_USER };
+}
+
+export function successCreateUser(currentUser) {
+  return { type: SUCCESS_CREATE_USER, payload: currentUser };
+}
+
+export function createUser(userData) {
+  const { firstName, lastName, email, password } = userData;
+  return async (dispatch) => {
+    dispatch(requestCreateUser());
+
+    const { user } = await auth.createUserWithEmailAndPassword(email, password);
+    await db.collection("users").doc(user.uid).set({
+      firstName,
+      lastName,
+    });
+
+    dispatch(successCreateUser(user.uid));
+  };
 }
--- scheduleit/src/store/reducers/authReducer.js
+++ scheduleit/src/store/reducers/authReducer.js
@@ -1,5 +1,19 @@
-function authReducer(state = { user: null }, action) {
+import {
+  REQUEST_CREATE_USER,
+  SUCCESS_CREATE_USER,
+} from "../constants/authConstants";
+
+const initialState = {
+  isLoading: false,
+  user: null,
+};
+function authReducer(state = initialState, action) {
   switch (action.type) {
+    case REQUEST_CREATE_USER:
+      return { ...state, isLoading: true };
+      break;
+    case SUCCESS_CREATE_USER:
+      return { ...state, isLoading: false, user: action.payload };
     default:
       return state;
   }
