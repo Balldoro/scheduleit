445bacc82b8a67d5ba21f55c733d9d7bcce448ee
Marek Miklaszewski <mar.mik1@wp.pl>
2020-07-24 12:07:46 +0200

    Add hiding Sidebar on mobile

--- scheduleit/src/components/Sidebar/Sidebar.js
+++ scheduleit/src/components/Sidebar/Sidebar.js
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import UserPanel from "./UserPanel/UserPanel";
-import { Container } from "./SidebarStyles";
+import { Container, CloseButton } from "./SidebarStyles";
 import Navigation from "./Navigation/Navigation";
 import { connect } from "react-redux";
 import {
@@ -8,6 +8,7 @@ import {
   deleteProject,
 } from "../../store/actions/projectsActions";
 import { logoutUser } from "../../store/actions/authActions";
+import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
 
 function Sidebar({
   uid,
@@ -16,16 +17,29 @@ function Sidebar({
   createProject,
   deleteProject,
 }) {
+  const [isOpen, setIsOpen] = useState(window.innerWidth >= 800);
+  window.addEventListener("resize", () => {
+    if (window.innerWidth >= 800) {
+      setIsOpen(true);
+    }
+  });
   return (
-    <Container>
-      <UserPanel userInfo={info} logOut={logOut} />
-      <Navigation
-        userProjects={projects}
-        createProject={createProject}
-        deleteProject={deleteProject}
-        uid={uid}
-      />
-    </Container>
+    <>
+      <CloseButton active={isOpen} onClick={() => setIsOpen(!isOpen)}>
+        {isOpen ? <FaChevronLeft /> : <FaChevronRight />}
+      </CloseButton>
+      {isOpen && (
+        <Container>
+          <UserPanel userInfo={info} logOut={logOut} />
+          <Navigation
+            userProjects={projects}
+            createProject={createProject}
+            deleteProject={deleteProject}
+            uid={uid}
+          />
+        </Container>
+      )}
+    </>
   );
 }
 
--- scheduleit/src/components/Sidebar/SidebarStyles.js
+++ scheduleit/src/components/Sidebar/SidebarStyles.js
@@ -9,3 +9,21 @@ export const Container = styled.div`
   padding: 1.25rem;
   z-index: 100;
 `;
+
+export const CloseButton = styled.button`
+  position: absolute;
+  font-size: 1.2rem;
+  border: none;
+  top: 50%;
+  left: ${(props) => (props.active ? "240px" : "0")};
+  font-size: 1.5rem;
+  background-color: #2fe797;
+  border-top-right-radius: 15px;
+  border-bottom-right-radius: 15px;
+  padding: 5px;
+  display: flex;
+  align-items: center;
+  @media (min-width: 800px) {
+    display: none;
+  }
+`;
