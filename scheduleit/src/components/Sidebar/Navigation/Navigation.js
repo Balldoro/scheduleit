67d36e8d81dee8076950d4dd2023e06097e8c460
Marek Miklaszewski <mar.mik1@wp.pl>
2020-07-24 15:02:54 +0200

    Add redirect when deleting project, add Daily link

--- scheduleit/src/components/Sidebar/Navigation/Navigation.js
+++ scheduleit/src/components/Sidebar/Navigation/Navigation.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Projects from "../Projects/Projects";
 import { ActionLink } from "./NavigationStyles";
-import { FaColumns } from "react-icons/fa";
+import { FaColumns, FaTasks } from "react-icons/fa";
 import { NavLink } from "react-router-dom";
 
 function Navigation({ userProjects, createProject, uid, deleteProject }) {
@@ -11,6 +11,12 @@ function Navigation({ userProjects, createProject, uid, deleteProject }) {
         <ActionLink as={NavLink} to="/dashboard" exact>
           <FaColumns style={{ marginRight: "0.5rem" }} />
           Dashboard
+        </ActionLink>
+      </li>
+      <li>
+        <ActionLink as={NavLink} to="/dashboard/daily" exact>
+          <FaTasks style={{ marginRight: "0.5rem" }} />
+          Daily
         </ActionLink>
       </li>
       <li>
--- scheduleit/src/components/Sidebar/Projects/ProjectItem/ProjectItem.js
+++ scheduleit/src/components/Sidebar/Projects/ProjectItem/ProjectItem.js
@@ -6,16 +6,26 @@ import {
   DeleteProjectButton,
 } from "./ProjectItemStyles";
 import { FaTrash } from "react-icons/fa";
+import { Redirect, useHistory } from "react-router-dom";
 
 function ProjectItem({ data: { name, color, id }, uid, deleteProject }) {
   const path = `/dashboard/${name.toLowerCase()}`;
+  const history = useHistory();
+
+  const handleDeleteProject = () => {
+    deleteProject(uid, id);
+    if (history.location.pathname === path) {
+      history.replace("/dashboard");
+    }
+  };
+
   return (
     <ItemContainer>
       <ProjectLink to={path}>
         <ProjectSign color={color} />
         {name}
       </ProjectLink>
-      <DeleteProjectButton onClick={() => deleteProject(uid, id)}>
+      <DeleteProjectButton onClick={handleDeleteProject}>
         <FaTrash />
       </DeleteProjectButton>
     </ItemContainer>
